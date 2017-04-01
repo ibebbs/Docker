b671c33fce579f64cf961ad41612698a0ca5ac28
Ian Bebbington <github@bebbs.co.uk>
2017-04-01 23:39:32 +0100

    Corrected entry point script to correctly start CoreNLP server

--- Nanoserver-CoreNLP/Start-CoreNLP.ps1
+++ Nanoserver-CoreNLP/Start-CoreNLP.ps1
@@ -1,7 +1,7 @@
 $targetDir = "C:\"
 $appDir = $targetDir + "\Apps"
 $coreNLPDir = $appDir + "\CoreNLP"
-$coreNLP = "%JAVA_HOME%\bin\Java.exe"
+$coreNLP = "$env:JAVA_HOME\bin\java.exe"
 
 Set-Location -Path $coreNLPDir
 &$coreNLP -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000
