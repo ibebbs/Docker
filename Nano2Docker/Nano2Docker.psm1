7bdc695fc9da5995d7fd688f16765f9a3ee62b68
Ian Bebbington <github@bebbs.co.uk>
2017-06-26 18:05:54 +0100

    Enhancements and bug fixes to the Nano2Docker module

--- Nano2Docker/Nano2Docker.psm1
+++ Nano2Docker/Nano2Docker.psm1
@@ -6,7 +6,7 @@ powershell C:\PrepareDocker.ps1
 
 $prepareDockerPowershell = @"
 Expand-Archive C:\docker.zip -DestinationPath $Env:ProgramFiles
-##Remove-Item -Force docker.zip
+Remove-Item -Force docker.zip
 
 $env:path += ";$env:ProgramFiles\docker"
 [Environment]::SetEnvironmentVariable("PATH", $env:path)
@@ -25,7 +25,7 @@ Start-Service docker
 "@
 
 function Initialize-Nano2DockerImage {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName="WebUpdate")]
     Param(
         [Parameter(Mandatory=$true, HelpMessage="Specifies the path of the source media. If a local copy of the source media already exists, and it is specified using the BasePath parameter, then no copying is performed.")]
         [string]$MediaPath,
@@ -33,8 +33,12 @@ function Initialize-Nano2DockerImage {
         [string]$BasePath,
         [Parameter(Mandatory=$false, HelpMessage="Url to Docker zip file")]
         [string]$DockerUrl="https://download.docker.com/components/engine/windows-server/17.03/docker-17.03.0-ee.zip",
-        [Parameter(Mandatory=$false, HelpMessage="Url to Docker zip file")]
-        [string]$UpdateUrl="http://download.windowsupdate.com/d/msdownload/update/software/updt/2017/05/windows10.0-kb4023680-x64_de5502ce899738bedd5eb20f10cfe67ea26ff5b6.msu"
+        [Parameter(Mandatory=$false, HelpMessage="Url to Docker zip file", ParameterSetName="WebUpdate")]
+        [string]$UpdateUrl="http://download.windowsupdate.com/d/msdownload/update/software/updt/2017/05/windows10.0-kb4023680-x64_de5502ce899738bedd5eb20f10cfe67ea26ff5b6.msu",
+        [Parameter(Mandatory=$false, HelpMessage="Url to Docker zip file", ParameterSetName="FileUpdate")]
+        [string]$UpdateFile,        
+        [Parameter(Mandatory=$false, HelpMessage="Password for the Administrator account of the Nano2Docker image")]
+        [SecureString]$Password
     )
 
     Import-Module "$($MediaPath)\NanoServer\NanoServerImageGenerator\NanoServerImageGenerator.psm1"
@@ -43,7 +47,10 @@ function Initialize-Nano2DockerImage {
         $BasePath = Convert-Path -Path .
     }
 
-    $cred = Get-Credential -UserName '~\Administrator' -Message "Enter the administrator password Nano2Docker image:"
+    if (($Password -eq $null) -or ($Password -eq "")) {
+        $cred = Get-Credential -UserName '~\Administrator' -Message "Enter the administrator password Nano2Docker image:"
+        $Password = $cred.Password
+    }
     
     $expand = "expand"
     
@@ -61,42 +68,56 @@ function Initialize-Nano2DockerImage {
     $name = "Nano2Docker"
     $diskPath = "$($BasePath)\$($name).vhdx"
 
-    if ($UpdateUrl -ne "") {
-        New-Item -Type Directory "$($BasePath)\Updates" -Force
+    if ($psCmdlet.ParameterSetName -eq "WebUpdate") {
+        New-Item -Type Directory "$($BasePath)\Updates" -ErrorAction SilentlyContinue
         Invoke-WebRequest -UseBasicparsing -Outfile "$($BasePath)\Updates\Update.msu" $UpdateUrl
-        &$expand "$($BasePath)\Updates\Update.msu" "$($BasePath)\Updates" -F:*.cab -R
-        $update = Get-ChildItem -Path "$($BasePath)\Updates" -Filter "*.cab" | 
-            Where-Object {$_.Name -ne "WSUSSCAN.cab"} | 
-            Select-Object FullName |
-            ForEach-Object { '"' + $_.FullName + '"' }
-        
-        $servicingPath = [System.String]::Join(", ", $update)
-
-        New-NanoServerImage -DeploymentType Guest -Edition Standard -MediaPath $MediaPath -BasePath $BasePath -TargetPath $diskPath -Containers -EnableRemoteManagementPort -CopyPath @($docker, $prepareDockerBatchFile, $prepareDockerPowershellFile) -SetupCompleteCommand "C:\PrepareDocker.bat" -ComputerName $name -AdministratorPassword $cred.Password -ServicingPackagePath $servicingPath
+        $UpdateFile = "$($BasePath)\Updates\Update.msu"
     } else {
-        New-NanoServerImage -DeploymentType Guest -Edition Standard -MediaPath $MediaPath -BasePath $BasePath -TargetPath $diskPath -Containers -EnableRemoteManagementPort -CopyPath @($docker, $prepareDockerBatchFile, $prepareDockerPowershellFile) -SetupCompleteCommand "C:\PrepareDocker.bat" -ComputerName $name -AdministratorPassword $cred.Password
+        Copy-Item $UpdateFile -Destination "$($BasePath)\Updates\Update.msu" -Force
+        $UpdateFile = "$($BasePath)\Updates\Update.msu"
     }
+    
+    &$expand $UpdateFile "$($BasePath)\Updates" -F:*.cab -R
+    $update = @(Get-ChildItem -Path "$($BasePath)\Updates" -Filter "*.cab" | 
+        Where-Object {$_.Name -ne "WSUSSCAN.cab"} | 
+        Select-Object FullName |
+        ForEach-Object { $_.FullName })
+    
+    $servicingPath = [System.String]::Join(", ", $update)
+
+    New-NanoServerImage -DeploymentType Guest -Edition Standard -MediaPath $MediaPath -BasePath $BasePath -TargetPath $diskPath -Containers -EnableRemoteManagementPort -CopyPath @($docker, $prepareDockerBatchFile, $prepareDockerPowershellFile) -SetupCompleteCommand "C:\PrepareDocker.bat" -ComputerName $name -AdministratorPassword $Password -ServicingPackagePath $servicingPath.ToString()
 }
 
 function New-Nano2Docker {
     [CmdletBinding()]
     Param(
+        [Parameter(Mandatory=$true, HelpMessage="Specifies the path of the source media. If a local copy of the source media already exists, and it is specified using the BasePath parameter, then no copying is performed.")]
         [string]$MediaPath,
-        [string]$BasePath,
+        [Parameter(Mandatory=$false, HelpMessage="Specifies the source Nano2Docker image created with Initialize-Nano2DockerImage")]
         [string]$ImagePath,
+        [Parameter(Mandatory=$false, HelpMessage="Specifies the name for the virtual machine")]
         [string]$VMName="Nano2Docker",
-        [string]$VMPath="C:\Users\Public\Documents\Hyper-V\Virtual Hard Disks"
+        [Parameter(Mandatory=$false, HelpMessage="Specifies the directory in which to save the new VHDX")]
+        [string]$VMPath="C:\Users\Public\Documents\Hyper-V\Virtual Hard Disks",
+        [Parameter(Mandatory=$false, HelpMessage="Specifies the number of processors to allocate to the virtual machine")]
+        [int]$VMProcessor=4
+        [Parameter(Mandatory=$false, HelpMessage="Password for the Administrator account of the virtual machine")]
+        [SecureString]$Password
     )
 
     Import-Module "$($MediaPath)\NanoServer\NanoServerImageGenerator\NanoServerImageGenerator.psm1"
     Import-Module Hyper-V
 
-    $cred = Get-Credential -UserName '~\Administrator' -Message "Enter the administrator password for $($VMName):"
+    if (($Password -eq $null) -or ($Password -eq "")) {
+        $cred = Get-Credential -UserName '~\Administrator' -Message "Enter the administrator password for $($VMName):"
+        $Password = $cred.Password
+    }
+    
     $diskPath = "$($VMPath)\$($VMName).vhdx"
 
     Copy-Item -Path $ImagePath -Destination $diskPath
 
-    Edit-NanoServerImage -MediaPath $MediaPath -BasePath $BasePath -TargetPath $diskPath -ComputerName $VMName -AdministratorPassword $cred.Password
+    Edit-NanoServerImage -TargetPath $diskPath -ComputerName $VMName -AdministratorPassword $Password
     New-VM -Name $VMName -Generation 2 -VHDPath $diskPath -BootDevice "VHD" -Path $VMPath -SwitchName (Get-VMSwitch).Name
 
     $vm = Get-VM -VMName $VMName
