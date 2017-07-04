ffb3ef7fa41159915a70f8057d6cf1ef44b8dfaf
Ian Bebbington <github@bebbs.co.uk>
2017-07-04 10:54:41 +0100

    Added New-Nano2DockerSwarm function

--- Nano2Docker/Nano2Docker.psm1
+++ Nano2Docker/Nano2Docker.psm1
@@ -145,4 +145,110 @@ function New-Nano2Docker {
     $ipAddress = $vm.NetworkAdapters[0].IPAddresses[0]
 
     Write-Host "$($VMName) running at $($ipAddress)"
+
+    return $vm
+}
+
+Function New-Nano2DockerSwarm {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true, HelpMessage="Specifies the path of the source media. If a local copy of the source media already exists, and it is specified using the BasePath parameter, then no copying is performed.")]
+        [string]$MediaPath,
+        [Parameter(Mandatory=$false, HelpMessage="Specifies the source Nano2Docker image created with Initialize-Nano2DockerImage")]
+        [string]$ImagePath,
+        [Parameter(Mandatory=$true, HelpMessage="Directory in which to store VM files")]
+        [string]$VMPath,
+        [Parameter(Mandatory=$false, HelpMessage="Prefix for the new hyper-v instance names")]
+        [string]$VMPrefix="b2n",
+        [Parameter(Mandatory=$false, HelpMessage="Number of manager nodes to create")]
+        [int]$ManagerNodes=1,
+        [Parameter(Mandatory=$false, HelpMessage="Number of worker nodes to create")]
+        [int]$WorkerNodes=3,
+        [Parameter(Mandatory=$false, HelpMessage="Number of worker nodes to create")]
+        [string]$DockerUrl="https://download.docker.com/components/engine/windows-server/17.03/docker-17.03.0-ee.zip"
+    )
+
+    $docker = "docker.exe"
+    $cred = Get-Credential -UserName '~\Administrator' -Message 'Enter the administrator password for the swarm:' 
+
+    $managerIPAddress = $null
+    $managerToken = $null
+    $workerToken = $null
+    
+    For ($managerCount = 0; $managerCount -lt $ManagerNodes; $managerCount++) {
+        $managerName = "$($VMPrefix)-mngr-$($managerCount)"
+        $managerDisk = "$($managerName).vhdx"
+        $managerDiskPath = "$($VMPath)\$($managerName)\$($managerDisk)"
+
+        Write-Output "Creating manager node named '$($managerName)' at '$($managerDiskPath)'"
+
+        $vm = New-Nano2Docker -MediaPath $MediaPath -ImagePath $ImagePath -VMName $managerName -VMPath $managerDiskPath -Password $cred.Password
+                
+        $ipAddress = $vm.NetworkAdapters[0].IPAddresses[0]
+
+        if ($managerCount -eq 0) {
+            $managerIPAddress = $ipAddress            
+            Write-Output "Initializing swarm on '$($managerName)' node at '$($ipAddress)'"
+            ## Initialise swarm
+            $params = @(
+                "-H $($ipAddress)", `
+                "swarm", `
+                "init", `
+                "--advertise-addr=$($ipAddress)", `
+                "--listen-addr=$($ipAddress):2377"
+            )
+            & $docker $params
+            ## Get manager token
+            $params = @(
+                "-H $($ipAddress)", `
+                "swarm", `
+                "join-token", `
+                "manager", `
+                "-q"
+            )
+            $managerToken = & $docker $params
+            ## Get worker token
+            $params = @(
+                "-H $($ipAddress)", `
+                "swarm", `
+                "join-token", `
+                "worker", `
+                "-q"
+            )
+            $workerToken = & $docker $params
+        } else {
+            Write-Output "'$($managerName)' node at '$($ipAddress)' joining swarm"
+            ## Join swarm as manager
+            $params = @(
+                "-H $($ipAddress)", `
+                "swarm", `
+                "join", `
+                "--token=$($managerToken)", `
+                $managerIPAddress
+            )
+            & $docker $params
+        }
+    }
+        
+    For ($workerCount = 0; $workerCount -lt $WorkerNodes; $workerCount++) {
+        $workerName = $VMPrefix + "-wrkr-" + $workerCount
+        $workerDisk = $workerName + ".vhdx"
+        $workerDiskPath = $VMPath + "\" + $workerDisk
+        
+        Write-Output "Creating worked node named '$($workerName)' at '$($workerDiskPath)'"
+        
+        $vm = New-Nano2Docker -MediaPath $MediaPath -ImagePath $ImagePath -VMName $workerName -VMPath $workerDiskPath -Password $cred.Password
+                
+        $ipAddress = $vm.NetworkAdapters[0].IPAddresses[0]
+
+        ## Join swarm as manager
+        $params = @(
+            "-H $($ipAddress)", `
+            "swarm", `
+            "join", `
+            "--token=$($workerToken)", `
+            $managerIPAddress
+        )
+        & $docker $params
+    }
 }
